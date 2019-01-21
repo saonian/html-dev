977425fb63ba11c4cd7a0e8c86c2ab29b1ca79be
刘凯 <liukai@rapoo.com>
2019-01-21 15:55:18 +0800

    增加postcss支持

--- build/tasks/sass.js
+++ build/tasks/sass.js
@@ -1,11 +1,19 @@
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
 module.exports = function (gulp, plugins, options) {
     const browserSync = require('browser-sync');
+    const postCssPlugins = [
+        autoprefixer({browsers: ['last 1 version']}),
+        cssnano()
+    ];
 
     return function (done) {
         gulp.src('src/css/**/*.scss')
             .pipe(plugins.plumber())
             .pipe(plugins.changed('dist/css'))
             .pipe(plugins.sass())
+            .pipe(plugins.postcss(postCssPlugins))
             .pipe(plugins.if(options.env === 'production', plugins.minifyCss()))
             .pipe(gulp.dest('dist/css'))
             .pipe(browserSync.reload({
